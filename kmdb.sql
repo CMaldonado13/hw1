fa2cb80b1a7a49fe0c585ea9fe162640129881f5
CMaldonado13 <christian.maldonado1@kellogg.northwestern.edu>
2023-01-19 20:33:40 +0000

    Created Tables

--- kmdb.sql
+++ kmdb.sql
@@ -101,9 +101,32 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS characters;
+DROP TABLE IF EXISTS prod_companies;
+DROP TABLE IF EXISTS actors;
 
 -- Create new tables, according to your domain model
 -- TODO!
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year_released INTEGER,
+    rating TEXT,
+    prod_company TEXT);
+CREATE TABLE characters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    movie TEXT,
+    actor TEXT,
+    rank INTEGER);
+CREATE TABLE prod_companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT);
+CREATE TABLE actors(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT);
+
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
